d171f3a92066b4df6e2443c7f687c49d6c9a57e4
Rajat Roy <rajatroy.dev@gmail.com>
2022-03-18 20:07:57 +0530

    Initial commit

---
+++ shareyourfood/__init__.py
@@ -0,0 +1,27 @@
+import logging
+
+import azure.functions as func
+
+import os
+from telegram.ext import Updater
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. The token is {os.getenv('TOKEN')}")
+    else:
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+        )
