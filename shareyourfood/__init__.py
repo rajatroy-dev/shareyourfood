49bb666d17ea490032270bb1ef7571e8ab8ba74c
Rajat Roy <rajatroy.dev@gmail.com>
2022-03-19 17:28:35 +0530

    Discovered a way to get location from user and test it with ngrok

--- shareyourfood/__init__.py
+++ shareyourfood/__init__.py
@@ -3,25 +3,32 @@ import logging
 import azure.functions as func
 
 import os
+from telegram import Update, Bot, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    token = os.getenv('TOKEN')
+    bot = Bot(token)
+    update = Update.de_json(req.get_json(), bot)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. The token is {os.getenv('TOKEN')}")
-    else:
-        return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
-        )
+    location_keyboard = KeyboardButton(
+        text="send_location", request_location=True)
+    contact_keyboard = KeyboardButton(
+        text="send_contact", request_contact=True)
+    custom_keyboard = [[location_keyboard, contact_keyboard]]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    # bot.send_message(chat_id=update.message.chat.id,
+    #                  text="Would you mind sharing your location and contact with me?",
+    #                  reply_markup=reply_markup)
+
+    bot.send_message(chat_id=update.message.chat.id,
+                     text=f"Echo: {update.message.text}",
+                     reply_markup=ReplyKeyboardRemove())
+
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+        status_code=200
+    )
