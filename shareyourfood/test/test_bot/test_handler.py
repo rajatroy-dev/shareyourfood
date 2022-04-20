b14eb6bf0dcab34ed55afc9c42734d226071b4c3
Rajat Roy <rajatroy.dev@gmail.com>
2022-04-20 08:02:48 +0530

    Handler unittests 2

--- shareyourfood/test/test_bot/test_handler.py
+++ shareyourfood/test/test_bot/test_handler.py
@@ -1,5 +1,7 @@
+from typing import Any, Iterable
 import unittest
 from unittest.mock import patch
+from shareyourfood.bot.constants import Constants
 
 from shareyourfood.bot.handler import Handle
 
@@ -9,6 +11,9 @@ class Test_Handler(unittest.TestCase):
     message_id: int
     full_name: str
     username: str
+    latitude: str
+    longitude: str
+    shares: Iterable[dict[str, Any]]
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -16,6 +21,13 @@ class Test_Handler(unittest.TestCase):
         cls.message_id = 123
         cls.full_name = 'full_name'
         cls.username = 'username'
+        cls.latitude = 11.11
+        cls.longitude = 12.12
+        cls.shares = [
+            {'username': 'dasd342'},
+            {'username': 'erter76'},
+            {'username': 'jtutgrh'}
+        ]
         return super().setUpClass()
 
     @classmethod
@@ -24,6 +36,9 @@ class Test_Handler(unittest.TestCase):
         cls.message_id = None
         cls.full_name = None
         cls.username = None
+        cls.latitude = None
+        cls.longitude = None
+        cls.shares = None
         return super().tearDownClass()
 
     @patch('shareyourfood.bot.handler.Conversation', autospec=True)
@@ -58,9 +73,8 @@ class Test_Handler(unittest.TestCase):
             username=cls.username,
             message_id=cls.message_id
         )
-        Converstion_instance.ask_location_for_share.assert_called_once_with(
-            chat_id=cls.chat_id
-        )
+        Converstion_instance.ask_location_for_share\
+            .assert_called_once_with(cls.chat_id)
 
     @patch('shareyourfood.bot.handler.Conversation', autospec=True)
     @patch('shareyourfood.bot.handler.Repository', autospec=True)
@@ -80,9 +94,8 @@ class Test_Handler(unittest.TestCase):
             username=cls.username,
             message_id=cls.message_id
         )
-        Converstion_instance.reply_server_error.assert_called_once_with(
-            chat_id=cls.chat_id
-        )
+        Converstion_instance.reply_server_error\
+            .assert_called_once_with(cls.chat_id)
 
     @patch('shareyourfood.bot.handler.Conversation', autospec=True)
     @patch('shareyourfood.bot.handler.Repository', autospec=True)
@@ -102,9 +115,8 @@ class Test_Handler(unittest.TestCase):
             username=cls.username,
             message_id=cls.message_id
         )
-        Converstion_instance.ask_location_for_request.assert_called_once_with(
-            chat_id=cls.chat_id
-        )
+        Converstion_instance.ask_location_for_request\
+            .assert_called_once_with(cls.chat_id)
 
     @patch('shareyourfood.bot.handler.Conversation', autospec=True)
     @patch('shareyourfood.bot.handler.Repository', autospec=True)
@@ -124,6 +136,170 @@ class Test_Handler(unittest.TestCase):
             username=cls.username,
             message_id=cls.message_id
         )
-        Converstion_instance.reply_server_error.assert_called_once_with(
-            chat_id=cls.chat_id
+        Converstion_instance.reply_server_error\
+            .assert_called_once_with(cls.chat_id)
+
+    @patch('shareyourfood.bot.handler.Conversation', autospec=True)
+    @patch('shareyourfood.bot.handler.Repository', autospec=True)
+    def test_location_repository_return_none(cls, Repository, Conversation):
+        Converstion_instance = Conversation.return_value
+        Repository_instance = Repository.return_value
+        Repository_instance.find_entry.return_value = {}
+        handle = Handle()
+        handle.location(cls.chat_id, cls.username,
+                        cls.message_id, cls.latitude, cls.longitude)
+        assert Repository.call_count == 1
+        assert Conversation.call_count == 1
+        assert Repository_instance.find_entry.call_count == 1
+        assert Repository_instance.save_share_food_details.call_count == 0
+        assert Converstion_instance.reply_shared_details_saved.call_count == 0
+        assert Converstion_instance.reply_server_error.call_count == 0
+        assert Repository_instance.find_food.call_count == 0
+        assert Converstion_instance.reply_nearby_shares.call_count == 0
+        assert Converstion_instance.no_share_found.call_count == 0
+        assert Converstion_instance.unknown_location.call_count == 1
+        Repository_instance.find_entry.assert_called_once_with(
+            chat_id=cls.chat_id,
+            username=cls.username,
+            message_id=cls.message_id - 2
         )
+        Converstion_instance.unknown_location\
+            .assert_called_once_with(cls.chat_id)
+
+    @patch('shareyourfood.bot.handler.Conversation', autospec=True)
+    @patch('shareyourfood.bot.handler.Repository', autospec=True)
+    def test_location_repository_return_share_repository_return_false(cls, Repository, Conversation):
+        Converstion_instance = Conversation.return_value
+        Repository_instance = Repository.return_value
+        Repository_instance.find_entry\
+            .return_value = {'message_type': Constants.SHARE}
+        Repository_instance.save_share_food_details.return_value = False
+        handle = Handle()
+        handle.location(cls.chat_id, cls.username,
+                        cls.message_id, cls.latitude, cls.longitude)
+        assert Repository.call_count == 1
+        assert Conversation.call_count == 1
+        assert Repository_instance.find_entry.call_count == 1
+        assert Repository_instance.save_share_food_details.call_count == 1
+        assert Converstion_instance.reply_shared_details_saved.call_count == 0
+        assert Converstion_instance.reply_server_error.call_count == 1
+        assert Repository_instance.find_food.call_count == 0
+        assert Converstion_instance.reply_nearby_shares.call_count == 0
+        assert Converstion_instance.no_share_found.call_count == 0
+        assert Converstion_instance.unknown_location.call_count == 0
+        Repository_instance.find_entry.assert_called_once_with(
+            chat_id=cls.chat_id,
+            username=cls.username,
+            message_id=cls.message_id - 2
+        )
+        Repository_instance.save_share_food_details.assert_called_once_with(
+            chat_id=cls.chat_id,
+            username=cls.username,
+            message_id=cls.message_id,
+            latitude=cls.latitude,
+            longitude=cls.longitude
+        )
+        Converstion_instance.reply_server_error\
+            .assert_called_once_with(cls.chat_id)
+
+    @patch('shareyourfood.bot.handler.Conversation', autospec=True)
+    @patch('shareyourfood.bot.handler.Repository', autospec=True)
+    def test_location_repository_return_share_repository_return_true(cls, Repository, Conversation):
+        Converstion_instance = Conversation.return_value
+        Repository_instance = Repository.return_value
+        Repository_instance.find_entry\
+            .return_value = {'message_type': Constants.SHARE}
+        Repository_instance.save_share_food_details.return_value = True
+        handle = Handle()
+        handle.location(cls.chat_id, cls.username,
+                        cls.message_id, cls.latitude, cls.longitude)
+        assert Repository.call_count == 1
+        assert Conversation.call_count == 1
+        assert Repository_instance.find_entry.call_count == 1
+        assert Repository_instance.save_share_food_details.call_count == 1
+        assert Converstion_instance.reply_shared_details_saved.call_count == 1
+        assert Converstion_instance.reply_server_error.call_count == 0
+        assert Repository_instance.find_food.call_count == 0
+        assert Converstion_instance.reply_nearby_shares.call_count == 0
+        assert Converstion_instance.no_share_found.call_count == 0
+        assert Converstion_instance.unknown_location.call_count == 0
+        Repository_instance.find_entry.assert_called_once_with(
+            chat_id=cls.chat_id,
+            username=cls.username,
+            message_id=cls.message_id - 2
+        )
+        Repository_instance.save_share_food_details.assert_called_once_with(
+            chat_id=cls.chat_id,
+            username=cls.username,
+            message_id=cls.message_id,
+            latitude=cls.latitude,
+            longitude=cls.longitude
+        )
+        Converstion_instance.reply_shared_details_saved\
+            .assert_called_once_with(cls.chat_id)
+
+    @patch('shareyourfood.bot.handler.Conversation', autospec=True)
+    @patch('shareyourfood.bot.handler.Repository', autospec=True)
+    def test_location_repository_return_request_repository_return_none(cls, Repository, Conversation):
+        Converstion_instance = Conversation.return_value
+        Repository_instance = Repository.return_value
+        Repository_instance.find_entry\
+            .return_value = {'message_type': Constants.REQUEST}
+        Repository_instance.find_food.return_value = None
+        handle = Handle()
+        handle.location(cls.chat_id, cls.username,
+                        cls.message_id, cls.latitude, cls.longitude)
+        assert Repository.call_count == 1
+        assert Conversation.call_count == 1
+        assert Repository_instance.find_entry.call_count == 1
+        assert Repository_instance.save_share_food_details.call_count == 0
+        assert Converstion_instance.reply_shared_details_saved.call_count == 0
+        assert Converstion_instance.reply_server_error.call_count == 0
+        assert Repository_instance.find_food.call_count == 1
+        assert Converstion_instance.reply_nearby_shares.call_count == 0
+        assert Converstion_instance.no_share_found.call_count == 1
+        assert Converstion_instance.unknown_location.call_count == 0
+        Repository_instance.find_entry.assert_called_once_with(
+            chat_id=cls.chat_id,
+            username=cls.username,
+            message_id=cls.message_id - 2
+        )
+        Repository_instance.find_food.assert_called_once_with(
+            latitude=cls.latitude,
+            longitude=cls.longitude
+        )
+        Converstion_instance.no_share_found\
+            .assert_called_once_with(cls.chat_id)
+
+    @patch('shareyourfood.bot.handler.Conversation', autospec=True)
+    @patch('shareyourfood.bot.handler.Repository', autospec=True)
+    def test_location_repository_return_request_repository_return_true(cls, Repository, Conversation):
+        Converstion_instance = Conversation.return_value
+        Repository_instance = Repository.return_value
+        Repository_instance.find_entry\
+            .return_value = {'message_type': Constants.REQUEST}
+        Repository_instance.find_food.return_value = cls.shares
+        handle = Handle()
+        handle.location(cls.chat_id, cls.username,
+                        cls.message_id, cls.latitude, cls.longitude)
+        assert Repository.call_count == 1
+        assert Conversation.call_count == 1
+        assert Repository_instance.find_entry.call_count == 1
+        assert Repository_instance.save_share_food_details.call_count == 0
+        assert Converstion_instance.reply_shared_details_saved.call_count == 0
+        assert Converstion_instance.reply_server_error.call_count == 0
+        assert Repository_instance.find_food.call_count == 1
+        assert Converstion_instance.reply_nearby_shares.call_count == 1
+        assert Converstion_instance.no_share_found.call_count == 0
+        assert Converstion_instance.unknown_location.call_count == 0
+        Repository_instance.find_entry.assert_called_once_with(
+            chat_id=cls.chat_id,
+            username=cls.username,
+            message_id=cls.message_id - 2
+        )
+        Repository_instance.find_food.assert_called_once_with(
+            latitude=cls.latitude,
+            longitude=cls.longitude
+        )
+        Converstion_instance.reply_nearby_shares\
+            .assert_called_once_with(cls.chat_id, cls.shares)
